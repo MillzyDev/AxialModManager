43e28d6916d6c78c5dc8905612b86c295e3bc509
Millzy <millzyyt08@gmail.com>
2023-09-17 23:20:54 +0100

    avalonia xplat template

---
+++ Axial/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Axial.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Axial/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Axial.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
