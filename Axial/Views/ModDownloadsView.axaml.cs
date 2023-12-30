5421525b587cf4749a220ae568e4c5afcdba3102
Millzy <millzyyt08@gmail.com>
2023-12-30 21:11:22 +0000

    placeholders

---
+++ Axial/Views/ModDownloadsView.axaml.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Axial.Views
+{
+    public partial class ModDownloadsView : UserControl
+    {
+        public ModDownloadsView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
+
---
+++ Axial/Views/ThunderstoreModsView.axaml.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Axial.Views
+{
+    public partial class ThunderstoreModsView : UserControl
+    {
+        public ThunderstoreModsView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
+
