a39b56eb4c0193bedcd59a999a69cc09ecdca3e1
Millzy <millzyyt08@gmail.com>
2023-12-30 21:11:14 +0000

    installed mods view

---
+++ Axial/Views/InstalledModsView.axaml.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Axial.Views
+{
+    public partial class InstalledModsView : UserControl
+    {
+        public InstalledModsView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
+
